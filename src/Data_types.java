fd4f18877317c38c90b9f39c9b9c50a1d3145193
Behnam <behnam.jabbari@aut.ac.ir>
2021-02-22 14:33:01 +0330

    Data Types

---
+++ src/Data_types.java
@@ -0,0 +1,20 @@
+public class Data_types {
+    public static void main(String[] args) {
+        char var_char = 'B';
+        String name = "Behnam";
+        boolean var_bool = true;
+        int var_int = 69;
+        double var_doub = 69.85;
+        System.out.print("My Charachter variable is: ");
+        System.out.println(var_char);
+        System.out.print("My String variable is: ");
+        System.out.println(name);
+        System.out.print("My Boolean variable is: ");
+        System.out.println(var_bool);
+        System.out.print("My Integer variable is: ");
+        System.out.println(var_int);
+        System.out.print("My double variable is: ");
+        System.out.println(var_doub);
+
+    }
+}
