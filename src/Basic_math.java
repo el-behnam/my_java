0065c99d73e21bee010c7315aa33427787015e8f
Behnam <behnam.jabbari@aut.ac.ir>
2021-02-22 17:06:47 +0330

    Basic Math

---
+++ src/Basic_math.java
@@ -0,0 +1,21 @@
+public class Basic_math {
+    public static void main(String[] args) {
+        int x = 32;
+        double y = 10;
+        double z = 2;
+        String s1 = "Hello ";
+        String s2 = "World!";
+        System.out.print("Simply 10/3: ");
+        System.out.println(10 / 3);
+        System.out.print("Simply 10.0/3: ");
+        System.out.println(10.0 / 3);
+        System.out.print("x is int, y is double. x / y: ");
+        System.out.println(x / y);
+        System.out.print("x = 32, y = 10, z = 2. x / y * z: ");
+        System.out.println(x / y * z);
+        System.out.print("x = 32, y = 10, z = 2. x + y * z: ");
+        System.out.println(x + y * z);
+        System.out.print("s1 = \"Hello \", s2 = \"World!\". s1 + s2: ");
+        System.out.println(s1 + s2);
+    }
+}
